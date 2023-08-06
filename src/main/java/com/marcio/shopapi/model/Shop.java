29f62ba8d35a1d5e34ce99151107db7b6be77312
Márcio Narcido D. <marcionarciso@live.com>
2023-08-06 12:43:57 -0300

    Criando as classes do modelo de domínio.

---
+++ src/main/java/com/marcio/shopapi/model/Shop.java
@@ -0,0 +1,59 @@
+package com.marcio.shopapi.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.marcio.shopapi.dto.ShopDTO;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Shop {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String identifier;
+	private String status;
+	
+	@Column(name = "date_shop")
+	@Temporal(TemporalType.DATE)
+	private LocalDate dateShop;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "shop")
+	private List<ShopItem> items;
+	
+	public static Shop convert(ShopDTO shopDto) {
+		Shop shop = new Shop();
+		shop.setIdentifier(shopDto.getIdentifier());
+		shop.setStatus(shopDto.getStatus());
+		shop.setDateShop(shopDto.getDateShop());
+		
+		// Converte os ShopItemDTO para ShopItem
+		List<ShopItem> itens = shopDto.getItems()
+										.stream()
+										.map(i -> ShopItem.convert(i))
+										.collect(Collectors.toList());
+									
+		
+		shop.setItems(itens);
+		
+		return shop;
+	}
+	
+}
