41c2147b049b878436b33985ede614190d8d1131
Márcio Narcido D. <marcionarciso@live.com>
2023-08-08 10:59:15 -0300

    Criando o Producer do kafka.

---
+++ src/main/java/com/marcio/shopapi/events/KafkaClient.java
@@ -0,0 +1,24 @@
+package com.marcio.shopapi.events;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import com.marcio.shopapi.dto.ShopDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaClient {
+
+	private final KafkaTemplate<String, ShopDTO> kafkaTemplate;
+	
+	private static final String SHOP_TOPIC_NAME = "SHOP_TOPIC";
+	
+	public void sendMessage(ShopDTO msg) {
+		kafkaTemplate.send(SHOP_TOPIC_NAME, msg);
+	}
+	
+}
