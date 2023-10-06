a902ce5d076e880ad11163c9778e00ae48914a4a
Márcio Narciso D <marcionarciso@live.com>
2023-10-06 16:57:18 -0300

    Corrigindo script de instalação do Docker.

--- scripts/docker/docker-build.sh
+++ scripts/docker/docker-build.sh
@@ -3,5 +3,5 @@
 # Compila o Dockerfile para criar a imagem
 
 echo "Compilando Dockerfile..."
-docker buildx build -t shop-api:1.0  $(dirname "$0")
+sudo docker buildx build -t shop-api:1.0  $(dirname "$0")
 echo "Imagem shop-api:1.0 criada."
